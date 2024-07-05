82e37f22b02fa0d2e5b2e66c5a305022682145c6
Awa_la_princesse <miss-ndeya>
2024-07-05 11:45:13 +0000

    mise a jour

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { SplitScreen } from "./SplitScreen";
-
-const LeftHandComponent = ({ name }) => {
-  return <h1 style={{ backgroundColor: "green" }}>{name}</h1>;
-};
-
-const RightHandComponent = ({ message }) => {
-  return <p style={{ backgroundColor: "red" }}>{message}!</p>;
-};
-
 function App() {
-  return (
-    <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="Shaun" />
-      <RightHandComponent message="Hello" />
-    </SplitScreen>
-  );
+  return <div>app</div>;
 }
 
 export default App;
--- src/people/LargePersonListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LargePersonListItem = () => {
-  return (
-    <div>LargePersonListItem</div>
-  )
-}
-
-export default LargePersonListItem
