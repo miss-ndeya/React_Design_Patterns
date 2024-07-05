695681ecbacef3c8602b8dcb73c7a40799e37bd0
Awa_la_princesse <miss-ndeya>
2024-07-05 12:40:13 +0000

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import { UncontrolledForm } from './UncontrolledForm';
+import { useState } from 'react';
+import { ControlledModal } from './ControlledModal';
+;
 
 function App() {
+	const [shouldShowModal, setShouldShowModal] = useState(false);
+
 	return (
-		<UncontrolledForm />
+		<>
+		<ControlledModal
+			shouldShow={shouldShowModal}
+			onRequestClose={() => setShouldShowModal(false)}
+		>
+			<h1>Hello!</h1>
+		</ControlledModal>
+		<button onClick={() => setShouldShowModal(!shouldShowModal)}>
+			{shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+		</button>
+		</>
 	);
 }
 
